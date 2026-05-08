9101cc6c7ddb0c3d56e6da88c9671164f3f7ac44
Tristan Kreindler <tristankreindler@gmail.com>
2026-05-08 01:28:06 -0500

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.cs
@@ -0,0 +1,67 @@
+public class Solution
+{
+    public string LongestPalindrome(string s)
+    {
+        return this.Helper(s).ToString();
+    }
+
+    private ReadOnlySpan<char> Helper(string s)
+    {
+        bool?[][] cache = new bool?[s.Length][];
+        for (int i = 0; i < s.Length; i++)
+        {
+            cache[i] = new bool?[s.Length];
+        }
+
+        ReadOnlySpan<char> max = [];
+
+        for (int low = 0; low < s.Length; low++)
+        {
+            for (int high = low + 1; high <= s.Length; high++)
+            {
+                int newLength = high - low;
+                if (newLength <= max.Length)
+                {
+                    continue;
+                }
+
+                if (this.IsPalindrome(s, low, newLength, cache))
+                {
+                    max = s.AsSpan(low, newLength);
+                }
+            }
+        }
+
+        return max;
+    }
+
+    private bool IsPalindrome(string s, int low, int length, bool?[][] cache)
+    {
+        if (cache[low][length - 1] is bool cachedIsPalindrome)
+        {
+            return cachedIsPalindrome;
+        }
+
+        bool isPalindrome = true;
+
+        ReadOnlySpan<char> segment = s.AsSpan(low, length);
+        if (segment[0] != segment[length - 1])
+        {
+            isPalindrome = false;
+        }
+
+        for (int left = 0; left < segment.Length / 2; left++)
+        {
+            int right = segment.Length - 1 - left;
+
+            if (segment[left] != segment[right])
+            {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        cache[low][length - 1] = isPalindrome;
+        return isPalindrome;
+    }
+}
