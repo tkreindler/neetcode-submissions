96a01d4634b5424bdb8cf0574eb331bb230b6a11
Tristan Kreindler <tristankreindler@gmail.com>
2026-06-30 16:33:42 -0700

    Add: implement-prefix-tree - submission-2

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-2.cpp
@@ -0,0 +1,84 @@
+class PrefixTree {
+
+    class Node
+    {
+      public:
+        Node()
+        {
+            this->is_end = false;
+            this->children.reserve(26);
+        }
+
+        bool is_end;
+        unordered_map<char, unique_ptr<Node>> children;
+    };
+
+    Node root;
+
+  public:
+    PrefixTree()
+    {
+    }
+    
+    void insert(string word)
+    {
+        Node* current = &this->root;
+
+        for (char letter : word)
+        {
+            unordered_map<char, unique_ptr<Node>>& children = current->children;
+            auto iter = children.find(letter);
+            if (iter != children.end())
+            {
+                // exists, use that
+                current = iter->second.get();
+            }
+            else
+            {
+                // create new
+                auto next = std::make_unique<Node>();
+                current = next.get();
+
+                children[letter] = std::move(next);
+            }
+        }
+
+        current->is_end = true; 
+    }
+
+    Node* search_helper(const string& word)
+    {
+        Node* current = &this->root;
+
+        for (char letter : word)
+        {
+            unordered_map<char, unique_ptr<Node>>& children = current->children;
+            auto iter = children.find(letter);
+            if (iter != children.end())
+            {
+                // exists, use that
+                current = iter->second.get();
+            }
+            else
+            {
+                // doesn't exist
+                return nullptr;
+            }
+        }
+
+        return current;
+    }
+    
+    bool search(const string& word)
+    {
+        Node* found = this->search_helper(word);
+        return found != nullptr &&
+            found->is_end;
+    }
+    
+    bool startsWith(const string& prefix)
+    {
+        Node* found = this->search_helper(prefix);
+        return found != nullptr;
+    }
+};
